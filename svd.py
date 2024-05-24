14ff50802d59bded9981d7d9920139c6806434d3
Pascal <kalezhang@hotmail.com>
2024-05-24 12:42:21 +0200

    comment

--- svd.py
+++ svd.py
@@ -5,6 +5,6 @@ U, S, V = np.linalg.svd(X)
 
 print(U*S@V)
 
-print(U[:,0].reshape(-1, 1)*S[0]@V[0,:].reshape(1, -1))
-
-print(U[:,0:3]*S[0:3]@V[0:3,:])
+print(U[:,0].reshape(-1, 1)*S[0]@V[0,:].reshape(1, -1))  #X approx with 1 elem
+ 
+print(U[:,0:2]*S[0:2]@V[0:2,:]) #X approx with 2 elem 
